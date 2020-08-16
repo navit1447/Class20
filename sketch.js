a76175861da9da04979a1a783e15c4b11974131f
navit1447 <69344864+navit1447@users.noreply.github.com>
2020-08-16 18:55:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var reck1,reck2
+function setup() {
+  createCanvas(800,400);
+reck1=createSprite(200,200,100,200)
+reck2=createSprite(400,200,100,200)
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
